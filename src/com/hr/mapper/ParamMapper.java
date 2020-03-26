5487765589617459d52dd3abcf6dbe3cdb684b13
xiaoran <303176152@qq.com>
2020-03-26 22:06:42 +0800

    mybatis获取不同参数值的方式

--- src/com/hr/mapper/ParamMapper.java
+++ src/com/hr/mapper/ParamMapper.java
@@ -1,5 +1,9 @@
 package com.hr.mapper;
 
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.hr.bean.Emp;
 
 public interface ParamMapper {
@@ -7,4 +11,15 @@ public interface ParamMapper {
 	//添加员工信息
 	void insertEmp(Emp emp);
 
+	//根据eid获取员工信息
+	Emp getEmpByEid(String eid);
+	
+	//根据eid和ename获取员工信息
+	Emp getEmpByEidAndEname(String eid, String ename);
+	
+	//根据map获取员工信息
+	Emp getEmpByMap(Map<String, Object> map);
+	
+	//根据eid和ename获取员工信息,通过注解@Param将eid放到map中，不需要手动map put添加了
+	Emp getEmpByEidAndEnameByParam(@Param("eid")String eid, @Param("ename")String ename);
 }
--- src/com/hr/test/TestParam.java
+++ src/com/hr/test/TestParam.java
@@ -2,6 +2,8 @@ package com.hr.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -23,7 +25,28 @@ public class TestParam {
 		//SqlSession sqlSession = sqlSessionFactory.openSession();//需要手动处理事务
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);//自动处理事务
 		ParamMapper mapper = sqlSession.getMapper(ParamMapper.class);
-		mapper.insertEmp(new Emp(null, "admin", 23, "男"));
+//		Emp emp = new Emp(null, "admin", 23, "男");
+//		mapper.insertEmp(emp);
+//		System.out.println(emp.getEid());
 		
+		//1.
+//		Emp emp = mapper.getEmpByEid("1");
+//		System.out.println(emp);
+		
+		//3.
+//		Emp emp = mapper.getEmpByEidAndEname("1", "小明");
+//		System.out.println(emp);
+		
+		//4.
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("eid", "1");
+//		map.put("ename", "小明");
+//		Emp emp = mapper.getEmpByMap(map);
+//		System.out.println(emp);
+		
+		//5.
+		//通过注解自动添加将键值添加，不需要手动添加
+		Emp emp = mapper.getEmpByEidAndEnameByParam("1", "小明");
+		System.out.println(emp);
 	}
 }
